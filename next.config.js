455c1784ba95d2c20649b949098a0ad506cea592
SahilTallnia <sahiltallnia222@gmail.com>
2023-03-24 01:31:36 +0530

    seo sitemap

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 /** @type {import('next').NextConfig} */
 
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
 module.exports = (phase) => {
   // devlopment mode variables
   if (phase === PHASE_DEVELOPMENT_SERVER) {
@@ -12,14 +10,27 @@ module.exports = (phase) => {
       reactStrictMode: true,
       env:{
         NEXT_PUBLIC_HOST:'http://localhost:3000'
-      }
+      },
+      async rewrites() {
+        return [
+          {
+            source: '/sitemap.xml',
+            destination: '/api/sitemap',
+          },
+        ]
+      },
     }
   }
 // production mode variables
   return {
     reactStrictMode: true,
-    env:{
-      NEXT_PUBLIC_HOST:'http://localhost:3000'
-    }
+    async rewrites() {
+      return [
+        {
+          source: '/sitemap.xml',
+          destination: '/api/sitemap',
+        },
+      ]
+    },
   }
 }
